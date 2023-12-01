eb0cd530c0503ce7d6b825aaf76cc1960175362f
Olanrewaju Farooq <olanrewajufarooq@yahoo.com>
2023-12-01 10:39:21 +0000

    Creating a source file with a format for collaboration

--- src/src_file.py
+++ src/src_file.py
@@ -1,1 +1,42 @@
-print("Hello world")
+import numpy as np
+from scipy.io import loadmat, savemat
+import PIL
+import cv2
+import argparse
+
+class Homography:
+    """This is a class strictly for computing homoegraphy.
+    
+    It involves a function to compute homography between only two images.
+    Either with or without RANSAC.    
+    """
+    
+    def __init__(self):
+        pass
+    
+    def compute_homography(self):
+        pass
+    
+    def run_ransac(self):
+        pass
+    
+    
+class FeatureExtraction:
+    """This is a class to perform feature extraction on images
+    """
+    def __init__(self):
+        pass
+
+class Solution:
+    def __init__(self):
+        self.read_config()
+        
+    def read_config(self):
+        pass
+    
+    def parse_config_data(self):
+        pass
+
+
+if __name__ == '__main__':
+    pass
